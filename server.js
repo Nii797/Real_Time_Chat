c58b1ab7a60705bbca844c402b7dcb7d43782fad
sonihidayatulloh797@gmail.com <sonihidayatulloh797@gmail.com>
2019-11-07 13:09:38 +0700

    update

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
-const io = require('socket.io')(3000)
+const io    = require('socket.io')(3300)
 
 const users = {}
 
 io.on('connection', socket => {
-    // console.log('New user')
-    // socket.emit('chat-message', 'Hello World')
-    socket.on('new-user', name => {
-        users[socket.id] = name
-        socket.broadcast.emit('user-connected', name) 
-    })
-    socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-    })
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('user-disconnected', users[socket.id])
-        delete users[socket.id]
-    //     users[socket.id] = name
-    //     socket.broadcast.emit('user-connected', name)
-    })
-})  
+    console.log('New User')
+    socket.emit('chat-message', 'Hello World')
+})
